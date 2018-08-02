7baf8b02edcd034d1bde3320c9413204a881a6be
xiaoj <xiaoj@kingnet.com>
2018-08-02 11:04:41 +0800

    test commit

--- dubbo-spring-boot-samples/dubbo-spring-boot-sample-consumer/src/main/java/com/alibaba/boot/dubbo/demo/consumer/bootstrap/DubboConsumerDemo.java
+++ dubbo-spring-boot-samples/dubbo-spring-boot-sample-consumer/src/main/java/com/alibaba/boot/dubbo/demo/consumer/bootstrap/DubboConsumerDemo.java
@@ -31,7 +31,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DubboConsumerDemo {
 
     public static void main(String[] args) {
-
+        System.out.println("bootstrap");
         SpringApplication.run(DubboConsumerDemo.class,args);
 
     }
